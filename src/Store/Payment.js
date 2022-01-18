b323de8c3031f7273cb155c8171dc47df0c47e9b
neustroev_aiaal <neustroev.aiaal@gmail.com>
2022-01-19 00:35:16 +0900

    изменил достижении

--- src/Store/Payment.js
+++ src/Store/Payment.js
@@ -30,8 +30,8 @@ module.exports = async function (req, res) {
             return res.status(200).json({ success : false, code: 2, error : "Неправильный идентификатор платежа" })
     }
     await db.mysqlInsert("INSERT INTO transactions SET ?", { user_id: user.id, operation: "+", type: type, detail_id: -1, amount : coinsCount })
-
-    AchievmentService(user.id, 20);
+    if (paymentIdentifier != "ads")
+        AchievmentService(user.id, 20);
     let transactions = await db.mysqlQuery('SELECT SUM(amount) FROM transactions WHERE user_id = ?', [user.id]);
         
     return res.status(200).json({ success : true, data: { fishcoins: transactions['SUM(amount)'] }, code: 1 })
